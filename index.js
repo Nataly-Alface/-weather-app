37e168c518013bd6fa59d7c50112d5386a630559
Nataly-Alface <natalyshavsha@gmail.com>
2022-07-26 04:04:40 +0300

    Updated index.html and index.js files

--- index.js
+++ index.js
@@ -25,64 +25,45 @@ formatDate();
 
 function displayTemperature(response) {
     console.log(response.data);
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
-  let humidityElement = document.querySelector("#humidity");
-  let windElement = document.querySelector("#wind");
-  let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("#icon");
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
 
-  celsiusTemperature = response.data.main.temp;
-
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-  cityElement.innerHTML = response.data.name;
-  descriptionElement.innerHTML = response.data.weather[0].description;
-  humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.wind.speed);
-  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+    celsiusTemperature = response.data.main.temp;
+    
     iconElement.setAttribute(
         "src",
         `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-  );
-  iconElement.setAttribute("alt", response.data.weather[0].description);
+    );
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 
-  getForecast(response.data.coord);
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    cityElement.innerHTML = response.data.name;
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    humidityElement.innerHTML = response.data.main.humidity;
+    windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  
+
+    // getForecast(response.data.coord);
 }
 
 //Search for a city
 
 function search(city) {
-let apiKey = "748628bc1246543ec7234048e2b022c6";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayTemperature);
+    let apiKey = "748628bc1246543ec7234048e2b022c6";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  let cityInputElement = document.querySelector("#searchInput");
-  search(cityInputElement.value);
-}
-
-function displayFahrenheitTemperature(event) {
     event.preventDefault();
-    let temperatureElement = document.querySelector("#temperature");
-    
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-  
-function displayCelsiusTemperature(event) {
-      event.preventDefault();
-      let temperatureElement = document.querySelector("#temperature");
-    
-      celsiusLink.classList.add("active");
-      fahrenheitLink.classList.remove("active");
-      let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-      temperatureElement.innerHTML = Math.round(celsiusTemperature);
+    let cityInputElement = document.querySelector("#searchInput");
+    search(cityInputElement.value);
 }
 
 let form = document.querySelector("#search-form");
@@ -106,7 +87,7 @@ function displayCelsiusTemperature(event) {
     
     celsiusLink.classList.add("active");
     fahrenheitLink.classList.remove("active");
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+    
     temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
