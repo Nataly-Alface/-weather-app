2786ac4556795b85785877c9da788f7c0c515ad0
Nataly-Alface <natalyshavsha@gmail.com>
2022-07-29 15:54:13 +0300

    Made changes in index.html and index.js files (remove html forecast code from html and added forecast function to js code

--- index.js
+++ index.js
@@ -21,6 +21,41 @@ function formatDate(date) {
     return currentDate;
 };
 formatDate();
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHtml = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    
+ 
+
+  forecastHtml =
+    forecastHtml + `
+      <div class="col-2">
+        <div class="weather-forecast-date">
+          ${day}
+        </div>
+        <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="" width="42"> 
+                            
+        <div class="weather-forecast-temperature">
+          <span class="weather-forecast-temperature-max">
+            18 °
+          </span> 
+          <span class="weather-forecast-temperature-min">
+            12 °
+          </span>
+        </div>                                   
+      </div>    
+  `;
+  })    
+ 
+  forecastHtml = forecastHtml + `</div>`;
+  forecastElement.innerHTML = forecastHtml;
+  //console.log(forecastHtml);
+
+}
     
 function displayTemperature(response) {
     console.log(response.data);
